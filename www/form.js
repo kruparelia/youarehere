962da232abc5bb1f78ad3b5935013e1bf0426f04
rhildred <rhildred@gmail.com>
2015-03-20 14:25:35 -0400

    added map for a you are here example

--- www/form.js
+++ www/form.js
@@ -1,12 +1,33 @@
-angular.module("myapp", [
-  'mobile-angular-ui',
+var app = angular.module("myapp", [
+    'mobile-angular-ui',
 
-  // touch/drag feature: this is from 'mobile-angular-ui.gestures.js'
-  // it is at a very beginning stage, so please be careful if you like to use
-  // in production. This is intended to provide a flexible, integrated and and
-  // easy to use alternative to other 3rd party libs like hammer.js, with the
-  // final pourpose to integrate gestures into default ui interactions like
-  // opening sidebars, turning switches on/off ..
-  'mobile-angular-ui.gestures'
-]).controller("mycontroller", function ($scope) {
+    // touch/drag feature: this is from 'mobile-angular-ui.gestures.js'
+    // it is at a very beginning stage, so please be careful if you like to use
+    // in production. This is intended to provide a flexible, integrated and and
+    // easy to use alternative to other 3rd party libs like hammer.js, with the
+    // final pourpose to integrate gestures into default ui interactions like
+    // opening sidebars, turning switches on/off ..
+    'mobile-angular-ui.gestures',
+    'uiGmapgoogle-maps'
+])
+
+app.config(function (uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        //    key: 'your api key',
+        v: '3.17',
+        libraries: 'weather,geometry,visualization'
+    });
+});
+
+app.controller("mycontroller", function ($scope, uiGmapGoogleMapApi) {
+    angular.extend($scope, {
+        init: function () {
+            uiGmapGoogleMapApi.then($scope.mapsReady);
+        },
+        mapsReady: function (maps) {
+        },
+        map: { center: { latitude: 45, longitude: -73 }, zoom: 8 }
+    }).init();
+
+
 });
