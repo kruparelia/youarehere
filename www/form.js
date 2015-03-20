c6507fea01cc5945bc1bf33f3fdab19f72a91585
rhildred <rhildred@gmail.com>
2015-03-20 13:37:34 -0400

    needed to inject dependencies into the angular module

--- www/form.js
+++ www/form.js
@@ -1,2 +1,12 @@
-angular.module("myapp", []).controller("mycontroller", function ($scope) {
+angular.module("myapp", [
+  'mobile-angular-ui',
+
+  // touch/drag feature: this is from 'mobile-angular-ui.gestures.js'
+  // it is at a very beginning stage, so please be careful if you like to use
+  // in production. This is intended to provide a flexible, integrated and and
+  // easy to use alternative to other 3rd party libs like hammer.js, with the
+  // final pourpose to integrate gestures into default ui interactions like
+  // opening sidebars, turning switches on/off ..
+  'mobile-angular-ui.gestures'
+]).controller("mycontroller", function ($scope) {
 });
